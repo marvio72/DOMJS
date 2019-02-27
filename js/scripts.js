4be25b22d0e190b7eb2d1a08a97e90bebfb9372d
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-02-27 15:57:28 -0600

    Reemplazando Elementos en JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,19 @@
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
   
-    //Eliminando Nodos
+    //Reemplazando Elementos
 
-    //Con querySelect
-    var primerPost = document.querySelector('main article');
-    primerPost.parentNode.removeChild(primerPost);
-    
-    //Con querySelectAll
-    var enlaces = document.querySelectorAll('#navegacion nav ul li a')[10];
-    enlaces.parentNode.removeChild(enlaces);
+    var viejoNodo = document.querySelector('main h2');
+    var nuevoNodo = document.querySelector('footer h2');
+    viejoNodo.parentNode.replaceChild(nuevoNodo, viejoNodo);
 
+    //Reemplazar nodo con un nuevo Elemento
+    var nuevoTitulo = document.createElement('H2');
+    var nuevoTexto = document.createTextNode('Hola Mundo');
+    nuevoTitulo.appendChild(nuevoTexto);
+
+    var segundoNodo = document.querySelectorAll('main h2')[1];
+    segundoNodo.parentElement.replaceChild(nuevoTitulo, segundoNodo);
   });
   
 })();
