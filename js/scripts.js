add73865998485e86c9d57e9cd0f5dfe245f845c
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-02-26 21:03:05 -0600

    Seleccionar por getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
+console.log("1");
+
+// Muy importante tener esta estructura para que lea todo el codigo
+// html y esto hace que el codigo javascript funcione una vez que 
+// cargue el codigo html
+
+// Estructura necesaria
 (function(){
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
+  
+    var logo = document.getElementById('logo');
     
-    
-      
+    var navegacion = document.getElementById('navegacion');  
       
     
     
   });
   
 })();
+console.log("3");
