beae8e5fe5161cfefc6e7a2e77340eb0276576cc
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-02-26 21:22:44 -0600

    Selector getElementsByClassName

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log("1");
-
 // Muy importante tener esta estructura para que lea todo el codigo
 // html y esto hace que el codigo javascript funcione una vez que 
 // cargue el codigo html
@@ -9,13 +7,19 @@ console.log("1");
   'use strict';
   document.addEventListener('DOMContentLoaded', function(){
   
-    var logo = document.getElementById('logo');
+     var navegacion = document.getElementsByClassName('navegacion');
+     console.log(navegacion[0]);
+
+     var contenido = document.getElementsByClassName('contenido');
+     console.log(contenido[0]);
     
-    var navegacion = document.getElementById('navegacion');  
-      
-    
+    //  Borra el contenido de div con clase contenido
+    contenido[0].style.display = 'none';
     
   });
   
 })();
-console.log("3");
+
+//La diferencia de getelementsByClassName vs getElementbyId es que
+// las clases pueden darnos mas de un resultado.
+//Recordemos que el id es unico por documento.
